fbabeddd443437f301888ba77b272a49bdb748fc
Matthew Meade Mabrey <Matthew@Matthews-MacBook-Air.local>
2017-02-08 15:08:23 -0600

    Testing commits/pushes

---
+++ Duck App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Duck App
+//
+//  Created by Matthew Meade Mabrey on 2/6/17.
+//  Copyright © 2017 Matthew Meade Mabrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
