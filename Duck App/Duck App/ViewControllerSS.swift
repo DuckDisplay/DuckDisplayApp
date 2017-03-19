4f94536b596befa574c4f6c52960bb312f55c72a
Austin Mills <austinmills@Austins-MacBook-Air.local>
2017-03-19 18:50:32 -0500

    Wrote function that refreshes screen saver background every 5 seconds with different duck image.

--- Duck App/Duck App/ViewControllerSS.swift
+++ Duck App/Duck App/ViewControllerSS.swift
@@ -10,10 +10,15 @@ import Foundation
 import UIKit
 
 class ViewScreenSaver : ViewController {
-
+    
+//Assign Variables Below
+    var timer: Timer!
+    var ducks = ["AmericanBlackDuck","AmericanWigeon","BlueWingedTeal"]
+    var ss = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(setBackground), userInfo: nil,repeats: true)
         
         setBackground()
         
@@ -22,12 +27,18 @@ class ViewScreenSaver : ViewController {
     
     override func setBackground() {
         UIGraphicsBeginImageContext(self.view.frame.size)
-        UIImage(named: "PairMallards")?.draw(in: self.view.bounds)
+        UIImage(named: ducks[ss])?.draw(in: self.view.bounds)
         
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        
         UIGraphicsEndImageContext()
         
         self.view.backgroundColor = UIColor(patternImage: image)
+        if ss <= 1 {
+            ss = ss+1
+        }
+        else {
+            ss = 0
+        }
     }
+    
 }
