181f6c7103edeb60c5bb234c5f6db752f8f55ad2
Austin Mills <austinmills@Austins-MacBook-Air.local>
2017-03-27 14:11:13 -0500

    Changed txt size on duck species card. Added Trivia game intro screen and am working on leaderboard UI.

--- Duck App/Duck App/ViewControllerDuckCard.swift
+++ Duck App/Duck App/ViewControllerDuckCard.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class ViewDuckCard : ViewController {
     override func viewDidLoad() {
-
+  //      navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Main Menu", style: .plain, target: self, action: #selector(addTapped))   
     }
 }
+
+    func addTapped(){
+    
+    }
