caa93a44ebfe9552e858e88fec4a12fa684504fa
John Cancilla <jpc0026@imacc.cse.eng.auburn.edu>
2017-03-29 13:43:23 -0500

    sbet background blurred background image for trivia screen 1.

---
+++ Duck App/Duck App/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Duck App
+//
+//  Created by John Cancilla on 3/29/17.
+//  Copyright © 2017 Auburn University. All rights reserved.
+//
+
+import Foundation
---
+++ Duck App/Duck App/Trivia Screen 1.swift
@@ -0,0 +1,54 @@
+//
+//  Trivia Screen 1.swift
+//  Duck App
+//
+//  Created by John Cancilla on 3/29/17.
+//  Copyright © 2017 Auburn University. All rights reserved.
+//
+
+import UIKit
+
+class Trivia_Screen_1: UIViewController {
+
+    override func viewDidLoad() {
+       setBackground()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setBackground() {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "mmbackground")?.draw(in: self.view.bounds)
+        
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        UIGraphicsEndImageContext()
+        
+        self.view.backgroundColor = UIColor(patternImage: image)
+
+        addBlurEffect()
+        
+    }
+    //Function to blur background
+    func addBlurEffect()
+    {
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.frame
+        
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight] // for supporting device rotation
+        view.addSubview(blurEffectView)
+        self.view.insertSubview(blurEffectView, at: 0)
+        
+    }
+
+
+}
+
+
