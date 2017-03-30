bbbbbfaf6d1a9b024cf9f75793a69665ebf5b53f
Matthew Meade Mabrey <Matthew@Matthews-MacBook-Air.local>
2017-03-30 17:56:45 -0500

    Screen saver is completed. Throws a warning, but still works. Also it will continue to re-present the screen saver screen even when the screen saver is the active view.

--- Duck App/Duck App/ViewControllerMain.swift
+++ Duck App/Duck App/ViewControllerMain.swift
@@ -8,53 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    
     @IBOutlet weak var mainMenuLImage: UIImageView! //left image view
     @IBOutlet weak var mainMenuRImage: UIImageView! //right image view
-
+    var timer: Timer!
     
     override func viewDidLoad() {
         
         setBackground()
-       
+        
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        timer = Timer.scheduledTimer(timeInterval: 120, target: self, selector: #selector(showScreenSaver), userInfo: nil, repeats: true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-   func setBackground() {
+    
+    func setBackground() {
         UIGraphicsBeginImageContext(self.view.frame.size)
         UIImage(named: "mmbackground")?.draw(in: self.view.bounds)
         
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-   
+        
         UIGraphicsEndImageContext()
         
         self.view.backgroundColor = UIColor(patternImage: image)
         mainMenuLImage.layer.borderWidth = 5
         mainMenuRImage.layer.borderWidth = 5
-    
+        
         addBlurEffect()
-    
-        }
+        
+    }
     
     //Function to blur background
-   func addBlurEffect()
+    func addBlurEffect()
     {
         
-       let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.regular)
-       let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.frame
         
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight] // for supporting device rotation
         view.addSubview(blurEffectView)
         self.view.insertSubview(blurEffectView, at: 0)
- 
+        
+    }
+    
+    //Function that detects touches and resets timer when detected
+    override func touchesBegan(_ touches: Set<UITouch>?, with event: UIEvent?) {
+        if touches != nil {
+            //reset timer
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 120, target: self, selector: #selector(showScreenSaver), userInfo: nil, repeats: true)
         }
+    }
     
-
-    
+    //Presents screen saver ViewController when timer runs out
+    func showScreenSaver() {
+        timer.invalidate()
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "ScreenSaverView") as! ViewController
+        
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
--- Duck App/Duck App/ViewControllerSS.swift
+++ Duck App/Duck App/ViewControllerSS.swift
@@ -12,13 +12,11 @@ import UIKit
 class ViewScreenSaver : ViewController {
     
 //Assign Variables Below
-    var timer: Timer!
     var ducks = ["AmericanBlackDuck","AmericanWigeon","BlueWingedTeal"]
     var ss = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(setBackground), userInfo: nil,repeats: true)
         
         setBackground()
         
