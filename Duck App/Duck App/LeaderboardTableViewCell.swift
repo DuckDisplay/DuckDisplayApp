75cac848a51b42599e1254167ebca7a9add13af6
Austin Mills <you@example.com>
2017-04-06 20:38:52 -0500

    Under Issue UI Leaderboard: Finally got the leaderboard UI to merge. Under Issue Leaderboard Alert: Made 2 alerts, one for new high score and one for GAME OVER

---
+++ Duck App/Duck App/LeaderboardTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  LeaderboardTableViewCell.swift
+//  Duck App
+//
+//  Created by Austin Mills on 4/2/17.
+//  Copyright © 2017 Auburn University. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var leaderboardPlace: UILabel!
+    @IBOutlet weak var leaderboardName: UILabel!
+    @IBOutlet weak var leaderboardState: UILabel!
+    @IBOutlet weak var leaderboardScore: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Duck App/Duck App/Users.swift
@@ -0,0 +1,28 @@
+//
+//  Users.swift
+//  Duck App
+//
+//  Created by Austin Mills on 4/2/17.
+//  Copyright © 2017 Auburn University. All rights reserved.
+//
+
+import UIKit
+
+class Users{
+    //MARK: Properties
+    var name: String
+    var score: String
+    var state: String
+    var place: String
+    
+    //MARK: Initilization
+    
+    init(place: String, name: String, state: String, score: String){
+    
+    self.name = name
+    self.score = score
+    self.state = state
+    self.place = place
+    }
+    
+}
