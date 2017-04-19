def72a269453e6045b6fb56554301e08b12cbd40
Mark Gallagher <mag0038@tigermail.auburn.edu>
2017-04-19 14:30:20 -0500

    Added Page Screen Protocol This protocol defines what every view controller used on Page View Controllers should implement.

---
+++ Duck App/Duck App/PageScreenProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  PageScreenProtocol.swift
+//  Duck App
+//
+//  This protocol defines what any view controller used as a page in a Page View Controller 
+//  should follow.
+//
+//  Created by Mark Gallagher Jr on 4/19/17.
+//  Copyright © 2017 Auburn University. All rights reserved.
+//
+
+import Foundation
+
+//  Define the PageScreenProtocol
+//      Should be implemented by any View Controller used as a page
+//      Needs a page index to determine which page this view controller is in
+protocol PageScreenProtocol {
+    var pageIndex: Int { get set }
+}
